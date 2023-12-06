a267397976562858e1290569ee515c6efe78f5e7
san2op <139362761+san2op@users.noreply.github.com>
2023-12-06 09:50:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import './App.css'
+
+class App extends React.Component {
+
+  state = {advice: ' '};
+
+  componentDidMount (){
+      this.fetchadvice();
+  }
+
+  fetchadvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then ((response) => {
+      const {advice} = response.data.slip;
+      console.log(advice);
+
+      this.setState ({advice : advice});
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  render() {
+    const {advice} = this.state;
+    return (
+      <div className='app'>
+        <div className='card'>
+           <h1 className='heading'>{advice}</h1>
+           <button className='button' onClick={this.fetchadvice}>New Qoutes</button>
+        </div>
+      </div>
+      
+    );
+  }
+}
+
+export default App
